2eef685b77d833e8f84a619b97970f69e75a4a20
Guennoune Abderrahim <aguennoune@outlook.com>
2023-03-01 03:51:27 +0100

    Url server updated

--- client/script.js
+++ client/script.js
@@ -86,17 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
 
-    // const response = await fetch('https://codex-im0y.onrender.com/', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({
-    //         prompt: data.get('prompt')
-    //     })
-    // })
-
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codex-s1ae.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
